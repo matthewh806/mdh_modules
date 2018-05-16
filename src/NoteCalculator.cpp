6f44f4668664f906e869dddc4e8690a7a84df7f8
matthewh806 <matthewh806@gmail.com>
2018-05-16 16:46:42 +0200

    Renaming + print new voltage to console

--- src/NoteCalculator.cpp
+++ src/NoteCalculator.cpp
@@ -1,27 +1,32 @@
 #include "NoteCalculator.hpp"
 #include <iostream>
+#include <map>
 
+const float c_4 = 261.626f;
+std::string notes[13] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"};
 
-struct MyModule : Module {
+// see note / freq table here: http://pages.mtu.edu/~suits/notefreqs.html
+
+struct NoteCalculatorModule : Module {
 	enum ParamIds {
-		PITCH_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
-		PITCH_INPUT,
+		VOLTAGE_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
 		NUM_OUTPUTS
 	};
 	enum LightIds {
-		BLINK_LIGHT,
 		NUM_LIGHTS
 	};
+    
+    double ratio = pow(2, 1.0/12.0);
+    float voltage = 0.0f;
 
-	float blinkPhase = 0.0;
-
-	MyModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	NoteCalculatorModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+    
 	void step() override;
 
 	// For more advanced Module features, read Rack's engine.hpp header file
@@ -31,45 +36,32 @@ struct MyModule : Module {
 };
 
 
-void MyModule::step() {
-    float deltaTime = engineGetSampleTime();
+void NoteCalculatorModule::step() {
+	 if(voltage == inputs[VOLTAGE_INPUT].value)
+         return;
     
-	// Compute the frequency from the pitch parameter and input
-	float pitch = params[PITCH_PARAM].value;
-	pitch += inputs[PITCH_INPUT].value;
-	pitch = clamp(pitch, -4.0f, 4.0f);
-	// The default pitch is C4
-	float freq = 261.626f * powf(2.0f, pitch);
-    std::cout << "Freq: " + std::to_string(freq) << std::endl;
+    voltage = inputs[VOLTAGE_INPUT].value;
+    std::cout << "voltage: " + std::to_string(voltage) << std::endl;
 
-    // TODO: convert the frequency to the a note (lookup table).
+    // TODO: convert the voltage to the a note (lookup table).
+    
+
+    // TODO: display the note on a display.
     // This class uses a display so check there for inspiration:
     // https://github.com/sebastien-bouffier/Bidoo/blob/e82a658a88e3c7a27b6fbc97bf2b3def486a6e04/src/DTROY.cpp
-    
-    // TODO: display the note on a display.
-    
-	// Blink light at 1Hz
-	blinkPhase += deltaTime;
-	if (blinkPhase >= 1.0f)
-		blinkPhase -= 1.0f;
-	lights[BLINK_LIGHT].value = (blinkPhase < 0.5f) ? 1.0f : 0.0f;
 }
 
 
 struct MyModuleWidget : ModuleWidget {
-	MyModuleWidget(MyModule *module) : ModuleWidget(module) {
+	MyModuleWidget(NoteCalculatorModule *module) : ModuleWidget(module) {
 		setPanel(SVG::load(assetPlugin(plugin, "res/MyModule.svg")));
         
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-
-		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, MyModule::PITCH_PARAM, -3.0, 3.0, 0.0));
         
-		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, MyModule::PITCH_INPUT));
-
-		addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, MyModule::BLINK_LIGHT));
+		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, NoteCalculatorModule::VOLTAGE_INPUT));
 	}
 };
 
@@ -78,4 +70,4 @@ struct MyModuleWidget : ModuleWidget {
 // author name for categorization per plugin, module slug (should never
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
-Model *modelMyModule = Model::create<MyModule, MyModuleWidget>("MDH Modules", "Note Calculator", "Note Calculator", UTILITY_TAG);
+Model *modelNoteCalculator = Model::create<NoteCalculatorModule, MyModuleWidget>("MDH Modules", "Note Calculator", "Note Calculator", UTILITY_TAG);
