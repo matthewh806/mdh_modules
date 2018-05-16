0b83895634c604bd96ec75366a5c90acb5e630b3
matthewh806 <matthewh806@gmail.com>
2018-05-17 00:30:10 +0200

    Print note to module display

--- src/NoteCalculator.cpp
+++ src/NoteCalculator.cpp
@@ -3,21 +3,7 @@
 #include <array>
 
 const float c_4 = 261.626f;
-const std::array<char, 2> notes_sharp[12] = {
-    {'c', '\0'},
-    {'c', '#'},
-    {'d', '\0'},
-    {'d', '#'},
-    {'e', '\0'},
-    {'f', '\0'},
-    {'f', '#'},
-    {'g', '\0'},
-    {'g', '#'},
-    {'a', '\0'},
-    {'a', '#'},
-    {'b', '\0'},
-};
-
+static const char *notes_sharp[12] = {"c", "c#", "d", "e", "f", "f#", "g", "g#", "a", "a#", "b"};
 
 // see note / freq table here: http://pages.mtu.edu/~suits/notefreqs.html
 
@@ -38,6 +24,8 @@ struct NoteCalculatorModule : Module {
     
     double ratio = 1.0 / 12.0;
     float pitch = 0.0f;
+    
+    const char *note_display;
 
 	NoteCalculatorModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
     
@@ -56,24 +44,51 @@ void NoteCalculatorModule::step() {
     
     pitch = inputs[PITCH_INPUT].value;
     float o = std::floor(pitch);
-    float s = std::floor((pitch - o) * 12.f);
+    int s = std::floor((pitch - o) * 12.f);
     
     float out = o + s / 12.f;
     
-    const char note = notes_sharp[(int)s][0];
+    note_display = notes_sharp[s];
     
-    std::cout << "freq: " + std::to_string(out);
-    std::cout << ", note: ";
-    std::cout << note << std::endl;
-
-    // TODO: display the note on a display.vco
-    // This class uses a display so check there for inspiration:
-    // https://github.com/luckyxxl/vcv_luckyxxl/blob/master/src/Quantize.cpp
+//    std::cout << "freq: " + std::to_string(out);
+//    std::cout << ", note: ";
+//    std::cout << note_display;
+//    std::cout << std::endl;
 }
 
+struct NoteDisplay : TransparentWidget {
+    NoteCalculatorModule *module;
+    
+    
+    NoteDisplay() {
+    }
+    
+    void draw(NVGcontext *vg) override {
+        const char *note_display = module->note_display;
+        
+        NVGcolor backgroundColor = nvgRGB(0x38, 0x38, 0x38);
+        NVGcolor borderColor = nvgRGB(0x10, 0x10, 0x10);
+        nvgBeginPath(vg);
+        nvgRoundedRect(vg, 0.0, 0.0, box.size.x,box.size.y, 5.0);
+        nvgFillColor(vg, backgroundColor);
+        nvgFill(vg);
+        nvgStrokeWidth(vg, 1.0);
+        nvgStrokeColor(vg, borderColor);
+        nvgStroke(vg);
+        
+        nvgFontSize(vg, 24);
+        nvgTextLetterSpacing(vg, 2.5);
+        
+        Vec textPos = Vec(4, 20);
+        NVGcolor textColor = nvgRGB(0xaf, 0xd2, 0x2c);
+        nvgFillColor(vg, textColor);
+        nvgText(vg, textPos.x, textPos.y, note_display, NULL);
+        nvgFillColor(vg, textColor);
+    }
+};
 
-struct MyModuleWidget : ModuleWidget {
-	MyModuleWidget(NoteCalculatorModule *module) : ModuleWidget(module) {
+struct NoteCalculatorWidget : ModuleWidget {
+	NoteCalculatorWidget(NoteCalculatorModule *module) : ModuleWidget(module) {
 		setPanel(SVG::load(assetPlugin(plugin, "res/MyModule.svg")));
         
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
@@ -82,6 +97,14 @@ struct MyModuleWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         
         addInput(Port::create<PJ301MPort>(Vec(33, 90), Port::INPUT, module, NoteCalculatorModule::PITCH_INPUT));
+        
+        {
+            NoteDisplay *display = new NoteDisplay();
+            display->module = module;
+            display->box.pos = Vec(14.0f, 160.0f);
+            display->box.size = Vec(60.0f, 56.0f);
+            addChild(display);
+        }
 	}
 };
 
@@ -90,4 +113,4 @@ struct MyModuleWidget : ModuleWidget {
 // author name for categorization per plugin, module slug (should never
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
-Model *modelNoteCalculator = Model::create<NoteCalculatorModule, MyModuleWidget>("MDH Modules", "Note Calculator", "Note Calculator", UTILITY_TAG);
+Model *modelNoteCalculator = Model::create<NoteCalculatorModule, NoteCalculatorWidget>("MDH Modules", "Note Calculator", "Note Calculator", UTILITY_TAG);
